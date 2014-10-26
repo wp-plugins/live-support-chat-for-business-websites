4bdf42146f76276f1a163eced170aeb150893c2d
imsupporting <imsupporting@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-26 09:51:05 +0000

    Getting ready for the business chat update
    git-svn-id: https://plugins.svn.wordpress.org/live-support-chat-for-business-websites/trunk@1014089 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- imsupporting-live-chat-plugin.php
+++ imsupporting-live-chat-plugin.php
@@ -1,10 +1,10 @@
 <?php
 /*
-Plugin Name: Live Support Chat Plugin
+Plugin Name: Live Chat Support Plugin
 Plugin URI: http://www.IMsupporting.com
-Description: Live support and live chat support tool for Wordpress. This is an advanced live chat tool for Business websites who are dedicated to supporting their customers.
+Description: Live Chat support and live support is a tool for Wordpress. This is an advanced live chat tool for Business websites who are dedicated to supporting their customers.
 Add live support to your website instantly with our easy software installation.  Our live support tool works instantly and doesnt require any downloads to support all hosted in the cloud, ready when you are.
-Version: 1.0.0.1
+Version: 1.0.0.2
 Author: IMsupporting
 Author URI: http://www.IMsupporting.com
 License: GPL
